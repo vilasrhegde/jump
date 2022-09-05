fb3d8485b9e0ea9320a2c19ee767995abc812ff9
vilasrhegde <vilasrhegde@gmail.com>
2022-09-05 18:56:34 +0530

    added movies

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
+var res=document.getElementById('result');
 var counter=0;
 function jump(){
     if(character.classList == "animate"){return}
@@ -11,13 +12,15 @@ function jump(){
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){ 
+
         block.style.animation = "none";
-        alert("Game Over!");
         counter=0;
-        block.style.animation = "block 1s infinite linear";
+        block.style.animation = "block 1.5s infinite linear";
     }else{
         counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+        res.innerHTML='';
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);     
     }
-}, 10);
+    
+}, 15);

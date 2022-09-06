1b2334f190f6cc07e62f331fba9b502a0e618008
vilasrhegde <vilasrhegde@gmail.com>
2022-09-06 11:27:41 +0530

    dark mode

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function jump(){
     character.classList.add("animate");
     setTimeout(function(){
         character.classList.remove("animate");
-    },300);
+    },500);
 }
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
@@ -22,5 +22,5 @@ var checkDead = setInterval(function() {
         res.innerHTML='';
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);     
     }
-    
+
 }, 15);
